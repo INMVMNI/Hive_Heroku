519e076fe7c2b0d8980d83a514992dbe37908488
INMVMNI <timothyLperkins@gmail.com>
2021-04-29 18:39:45 -0700

    how

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
 // Put all API endpoints under '/api'
 app.get('/api/words', (req, res) => {
@@ -18,9 +18,9 @@ app.get('/api/words', (req, res) => {
   console.log(`Sent ${words} words`);
 });
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -40,9 +40,18 @@ if (process.env.NODE_ENV === 'production') {
 //   });
 // });
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
