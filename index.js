2df66ba46e151d337ee8aa9281513189c4b972be
INMVMNI <timothyLperkins@gmail.com>
2021-04-30 15:01:56 -0700

    jandaf

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/api/words', (req, res) => {
 // app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/client/src/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
